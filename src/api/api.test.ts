bb562f7d06e3364354b28bbf5002f011220b2163
Nickolay Dubinin <SuperDubPro@gmail.com>
2024-01-21 19:32:56 +0300

    readme and tests

---
+++ src/api/api.test.ts
@@ -0,0 +1,47 @@
+import request from 'supertest'
+import { app } from '../app'
+import { TrackTypes, type PostTrack, type PostUser, UserRoles } from '../types'
+
+describe('api', () => {
+  it('should create track', async () => {
+    // Given
+    const track: PostTrack = {
+      name: 'MIKS',
+      type: TrackTypes.kart,
+      configs: [{ img: 'rgdjhgf-hjkg-wqF-WASFDGX-EFDSG' }],
+      rating: {
+        value: 0
+      }
+    }
+    // When
+    const response = await request(app)
+      .post('tracks')
+      .send(track)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+    // Then
+    response
+      .expect(200)
+      .expect(response.body)
+      .toEqual(track)
+  })
+
+  it('should create user', async () => {
+    // Given
+    const user: PostUser = {
+      nickName: 'Stasyao San',
+      role: UserRoles.user
+    }
+    // When
+    const response = await request(app)
+      .post('tracks')
+      .send(user)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+    // Then
+    response
+      .expect(200)
+      .expect(response.body)
+      .toEqual(user)
+  })
+})
---
+++ src/types/api/common.ts
@@ -0,0 +1,5 @@
+export type IdType = string | number
+
+export enum IdKeys {
+  mongoose = '_id'
+}
